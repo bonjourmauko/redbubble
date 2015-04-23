a1b6e12a17d2eb2c41c0719097c2a36b42c1b799
maukoquiroga <maukoquiroga@gmail.com>
2015-04-23 13:40:45 +0200

    pending acceptance tests

--- spec/acceptance/redbubble_spec.rb
+++ spec/acceptance/redbubble_spec.rb
@@ -1,15 +1,29 @@
 describe 'redbubble' do
-  context 'default use' do
-    xit "assumes 'works.xml' is in the current path and tries to generate the HTML file(s)" do
+  describe 'generates the HTML file(s)' do
+    xit "assumes 'works.xml' is in the current path" do
       run_simple 'redbubble'
-      expect(all_output).to match(%r{works.xml})
+      expect(all_output).to match(/success/)
       assert_exit_status(0)
     end
 
-    xit 'takes user provided works file path and tries to generate the HTML file(s)' do
+    xit 'takes user provided works file path' do
       run_simple 'redbubble -f ../input.xml'
-      expect(all_output).to match(%r{input.xml})
+      expect(all_output).to match(/success/)
       assert_exit_status(0)
+    end
+  end
+
+  describe 'fails' do
+    xit 'the file path is incorrect' do
+      run_simple 'redbubble'
+      expect(all_output).to match(/error/)
+      assert_exit_status(1)
+    end
+
+    xit 'the file structure is incorrect' do
+      run_simple 'redbubble'
+      expect(all_output).to match(/error/)
+      assert_exit_status(1)
     end
   end
 end
