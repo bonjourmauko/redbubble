e72ea692697d7f69b029780571eb6bd76bcb00f4
maukoquiroga <maukoquiroga@gmail.com>
2015-04-28 02:59:06 +0200

    aruba

--- spec/support/aruba.rb
+++ spec/support/aruba.rb
@@ -7,9 +7,27 @@ RSpec.configure do |config|
   config.before(:each) do
     restore_env
     clean_current_dir
+
+    if RUBY_PLATFORM =~ /java/ || defined?(Rubinius)
+      @aruba_timeout_seconds = 60
+    else
+      @aruba_timeout_seconds = 10
+    end
   end
 end
 
 root = File.expand_path('../../..', __FILE__)
 
 ENV['PATH'] = "#{File.join(root, 'ext')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
+
+Aruba.configure do |config|
+  config.before_cmd do
+    set_env('JRUBY_OPTS', "-X-C #{ENV['JRUBY_OPTS']}") # disable JIT since these processes are so short lived
+  end
+end if RUBY_PLATFORM == 'java'
+
+Aruba.configure do |config|
+  config.before_cmd do
+    set_env('RBXOPT', "-Xint=true #{ENV['RBXOPT']}") # disable JIT since these processes are so short lived
+  end
+end if defined?(Rubinius)
